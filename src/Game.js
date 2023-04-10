6bd1be989796fa5753f36abf1712ef80292cd947
Alex Zinkevych <alexdarkmm@gmail.com>
2023-04-10 23:40:50 +0300

    refactor: minor improvements and suggestions

--- src/Game.js
+++ src/Game.js
@@ -1,91 +1,110 @@
+const QWERTY_KEYBOARD = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+
 class Game {
-  words = [
-    "word",
-    "cat",
-    "dog",
-    "read",
-    "vintorez",
-    "alexeychikk",
-    "andrey",
-    "nikitos",
-  ];
-  guessedWords = [];
-  usedWord = "";
-  guessedChar = "";
-  turnCount = 0;
+	words = [
+		"word",
+		"cat",
+		"dog",
+		"read",
+		"vintorez",
+		"alexeychikk",
+		"andrey",
+		"nikitos",
+	];
+	guessedWords = [];
+	currentWord = "";
+	guessedChar = "";
+	triesCount = 0;
 
-  getNewStartString() {
-    const randomArrayIndex = _.random(0, this.words.length - 1);
-    if (this.words.length === 0) {
-      throw new Error("No words available");
-    }
-    const randomWord = this.words[randomArrayIndex];
-    this.usedWord = randomWord;
-    document.getElementById("string-with-word").textContent =
-      randomWord.replace(/[a-z]/gi, "_ ");
-    this.guessedWords.push(this.words[randomArrayIndex]);
-    this.words.splice(randomArrayIndex, 1);
-  }
+	constructor() {
+		document
+			.getElementById("playButton")
+			.addEventListener("click", () => this.startGame());
+		document
+			.getElementById("nextWordButton")
+			.addEventListener("click", () => this.getNewWord());
+	}
 
-  createAlphabetButtons() {
-    let alphabetElement = document.getElementById("ABC");
-    let alphabetInnerHtml = "";
-    for (let i = 0; i < 26; i++) {
-      alphabetInnerHtml += `<button class="alphabet-char" id="alphabet-char-${
-        i + 97
-      }">${String.fromCharCode(i + 97)}</button>`;
-    }
-    alphabetElement.innerHTML = alphabetInnerHtml;
-  }
+	startGame() {
+		this.refreshWordToGuess();
+		this.createScreenKeyboard();
+		document.getElementById(
+			"triesCountLabel"
+		).textContent = `(${this.triesCount})`;
+		document.getElementById("playButton").textContent = "Restart";
+		document.getElementById("game").classList.add("started");
+	}
 
-  startGame() {
-    this.getNewStartString();
-    this.createAlphabetButtons();
-    this.registerEvents();
-    document.getElementById("turn-count").textContent = `(${this.turnCount})`;
-    document.getElementById("play").textContent = "Restart";
-    document.getElementById("next-word").style.display = "block";
-  }
+	refreshWordToGuess() {
+		if (this.words.length === 0) {
+			throw new Error("No words available");
+		}
+		const randomArrayIndex = _.random(0, this.words.length - 1);
+		const [randomWord] = this.words.splice(randomArrayIndex, 1);
+		this.currentWord = randomWord;
+		document.getElementById("currentWord").textContent = randomWord.replace(
+			/[a-z]/gi,
+			"_ "
+		);
+		this.guessedWords.push(randomWord);
+	}
 
-  getNewWord() {
-    try {
-      this.getNewStartString();
-      this.turnCount = 0;
-      document.getElementById("turn-count").textContent = `(${this.turnCount})`;
-    } catch {
-      alert("You completed the game");
-    }
-  }
+	createScreenKeyboard() {
+		let screenKeyboardEl = document.getElementById("screenKeyboard");
+		let keyboardHtml = QWERTY_KEYBOARD.map(
+			(row) =>
+				`<div class="keyboard-row">${row
+					.split("")
+					.map(
+						(char) =>
+							`<button class="keyboard-key" data-key="${char}">${char}</button>`
+					)
+					.join("")}
+      </div>`
+		).join("");
 
-  searchMatches(alphabetChar) {
-    if (this.usedWord.split("").indexOf(alphabetChar) !== -1) {
-      this.replaceLetters(alphabetChar);
-    }
-  }
+		screenKeyboardEl.innerHTML = keyboardHtml;
+		screenKeyboardEl.querySelectorAll(".keyboard-key").forEach((keyEl) => {
+			keyEl.addEventListener("click", () => {
+				this.handleKeyButtonClick(keyEl.dataset.key);
+			});
+		});
+	}
 
-  replaceLetters(alphabetChar) {
-    this.guessedChar += alphabetChar;
-    let regex = new RegExp(`[^${this.guessedChar}]`, "gi");
-    document.getElementById("string-with-word").textContent =
-      this.usedWord.replace(regex, " _ ");
-  }
+	getNewWord() {
+		try {
+			this.refreshWordToGuess();
+			this.triesCount = 0;
+			document.getElementById(
+				"triesCountLabel"
+			).textContent = `(${this.triesCount})`;
+		} catch {
+			alert("You completed the game");
+		}
+	}
 
-  handleClickButtonWithAlphabetChar(alphabetChar) {
-    if (
-      document.getElementById("string-with-word").textContent.indexOf("_") ===
-      -1
-    ) {
-      alert("You guessed the word");
-      return;
-    }
-    document.getElementById("turn-count").textContent = `(${++this.turnCount})`;
-    this.searchMatches(alphabetChar);
-  }
+	searchMatches(alphabetChar) {
+		if (this.currentWord.split("").indexOf(alphabetChar) !== -1) {
+			this.replaceLetters(alphabetChar);
+		}
+	}
 
-  registerEvents() {
-    _.times(26, (i) => {
-      const el = document.getElementById(`alphabet-char-${i + 97}`);
-      el.onclick = () => this.handleClickButtonWithAlphabetChar(el.textContent);
-    });
-  }
+	replaceLetters(alphabetChar) {
+		this.guessedChar += alphabetChar;
+		let regex = new RegExp(`[^${this.guessedChar}]`, "gi");
+		document.getElementById("currentWord").textContent =
+			this.currentWord.replace(regex, " _ ");
+	}
+
+	handleKeyButtonClick(char) {
+		if (
+			document.getElementById("currentWord").textContent.indexOf("_") === -1
+		) {
+			alert("You guessed the word");
+			return;
+		}
+		document.getElementById("triesCountLabel").textContent = `(${++this
+			.triesCount})`;
+		this.searchMatches(char);
+	}
 }
